095f7abb1aea3c6cb91be7bc5beca43b43e8db53
MatheusBenetti <mbbenetti1@gmail.com>
2025-08-07 16:23:27 -0300

    Added user routes

--- api/routes.go
+++ api/routes.go
@@ -3,10 +3,11 @@ package api
 import (
 	akumanomi "github.com/MatheusBenetti/one-piece-api/internal/akuma-no-mi"
 	"github.com/MatheusBenetti/one-piece-api/internal/pirate"
+	"github.com/MatheusBenetti/one-piece-api/internal/user"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(akumaNoMiHandler *akumanomi.AkumaNoMiHandler, pirateHandler *pirate.PirateHandler) *gin.Engine {
+func SetupRoutes(akumaNoMiHandler *akumanomi.AkumaNoMiHandler, pirateHandler *pirate.PirateHandler, userHandler *user.UserHandler) *gin.Engine {
 	router := gin.Default()
 
 	akumaNoMiGroup := router.Group("/akumanomi")
@@ -23,6 +24,12 @@ func SetupRoutes(akumaNoMiHandler *akumanomi.AkumaNoMiHandler, pirateHandler *pi
 		piratesGroup.GET("/:name", pirateHandler.FindByName)
 		piratesGroup.GET("/id/:id", pirateHandler.FindById)
 		piratesGroup.GET("/", pirateHandler.GetAllPirates)
+	}
+
+	userGroup := router.Group("/user")
+	{
+		userGroup.POST("/register", userHandler.RegisterUser)
+		userGroup.POST("/login", userHandler.Login)
 	}
 
 	return router
