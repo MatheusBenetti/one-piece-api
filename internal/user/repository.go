3a9d98a6ab247f831183093ca8f373dbcbfb51c3
MatheusBenetti <mbbenetti1@gmail.com>
2025-08-07 16:25:07 -0300

    Created user repository

---
+++ internal/user/repository.go
@@ -0,0 +1,24 @@
+package user
+
+import "gorm.io/gorm"
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewPirateRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Save(user *User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *UserRepository) FindByEmail(email string) (*User, error) {
+	var user User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
