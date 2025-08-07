a03f6e9f304e4ec5b5e9a7a823a6ce07857f3f22
MatheusBenetti <mbbenetti1@gmail.com>
2025-08-07 16:25:02 -0300

    Created user model

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,8 +1,11 @@
 package user
 
+import "gorm.io/gorm"
+
 type User struct {
-	ID       uint `json:"id" gorm:"primaryKey"`
-	username string
-	password string
-	email    string
+	gorm.Model
+	Username string `json:"username" gorm:"unique"`
+	Email    string `json:"email" gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	// Role     string `json:"role" gorm:"default:'player'"`
 }
