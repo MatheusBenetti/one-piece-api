a595747965226d69e4dd832e0282c88d94e8b678
MatheusBenetti <mbbenetti1@gmail.com>
2025-08-08 12:26:15 -0300

    Add Haki to migrations

--- config/migrations.go
+++ config/migrations.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	akumaNoMi "github.com/MatheusBenetti/one-piece-api/internal/akuma-no-mi"
+	haki "github.com/MatheusBenetti/one-piece-api/internal/haki"
 	pirate "github.com/MatheusBenetti/one-piece-api/internal/pirate"
+	piratehaki "github.com/MatheusBenetti/one-piece-api/internal/pirate-haki"
 	user "github.com/MatheusBenetti/one-piece-api/internal/user"
 	"gorm.io/gorm"
 )
 
 func MigrateAll(db *gorm.DB) error {
-	return db.AutoMigrate(&akumaNoMi.AkumaNoMi{}, &pirate.Pirate{}, &user.User{})
+	return db.AutoMigrate(&akumaNoMi.AkumaNoMi{}, &haki.Haki{}, &pirate.Pirate{}, &piratehaki.PirateHaki{}, &user.User{})
 }
