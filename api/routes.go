7163d886d9ef12fae73c7d5d5eb67b863cd73623
MatheusBenetti <mbbenetti1@gmail.com>
2025-08-18 17:16:05 -0300

    Added new routes

--- api/routes.go
+++ api/routes.go
@@ -2,13 +2,20 @@ package api
 
 import (
 	akumanomi "github.com/MatheusBenetti/one-piece-api/internal/akuma-no-mi"
+	haki "github.com/MatheusBenetti/one-piece-api/internal/haki"
 	pirate "github.com/MatheusBenetti/one-piece-api/internal/pirate"
+	pirateHaki "github.com/MatheusBenetti/one-piece-api/internal/pirate-haki"
 	user "github.com/MatheusBenetti/one-piece-api/internal/user"
 	auth "github.com/MatheusBenetti/one-piece-api/pkg/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(akumaNoMiHandler *akumanomi.AkumaNoMiHandler, pirateHandler *pirate.PirateHandler, userHandler *user.UserHandler) *gin.Engine {
+func SetupRoutes(
+	akumaNoMiHandler *akumanomi.AkumaNoMiHandler,
+	hakiHandler *haki.HakiHandler,
+	pirateHandler *pirate.PirateHandler,
+	pirateHakiHandler *pirateHaki.PirateHakiHandler,
+	userHandler *user.UserHandler) *gin.Engine {
 	router := gin.Default()
 
 	akumaNoMiGroup := router.Group("/akumanomi")
@@ -19,12 +26,25 @@ func SetupRoutes(akumaNoMiHandler *akumanomi.AkumaNoMiHandler, pirateHandler *pi
 		akumaNoMiGroup.GET("/", akumaNoMiHandler.GetAllAkumaNoMi)
 	}
 
+	hakiGroup := router.Group("/haki")
+	{
+		hakiGroup.POST("/", hakiHandler.CreateHaki, auth.AuthMiddleware())
+		hakiGroup.GET("/", hakiHandler.GetAllHakis)
+	}
+
 	piratesGroup := router.Group("/pirate")
 	{
 		piratesGroup.POST("/", pirateHandler.AddPirate, auth.AuthMiddleware())
 		piratesGroup.GET("/:name", pirateHandler.FindByName)
 		piratesGroup.GET("/id/:id", pirateHandler.FindById)
 		piratesGroup.GET("/", pirateHandler.GetAllPirates)
+		piratesGroup.GET("/:id/hakis", pirateHakiHandler.GetHakisByPirateId)
+	}
+
+	pirateHakiGroup := router.Group("/pirate-hakis")
+	{
+		pirateHakiGroup.POST("/assign", pirateHakiHandler.AssignHakiToPirate, auth.AuthMiddleware())
+		pirateHakiGroup.PUT("/update-level", pirateHakiHandler.UpdateHakiLevel, auth.AuthMiddleware())
 	}
 
 	userGroup := router.Group("/user")
